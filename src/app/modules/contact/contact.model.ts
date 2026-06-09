2fb359c944e3d52f6ed7a0bffedeb7efe56f42e0
Moshfiqur Rahman <studentemam@gmail.com>
2026-06-09 10:03:43 +0600

    feat: add contact model

---
+++ src/app/modules/contact/contact.model.ts
@@ -0,0 +1,36 @@
+import { model, Schema } from "mongoose";
+import { softDeletePlugin } from "../../../DB/plugins/softDeletePlugin";
+import { TContact, TContactModel } from "./contact.interface";
+
+const contactSchema = new Schema<TContact, TContactModel>(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: false,
+    },
+    email: {
+      type: String,
+      required: false,
+    },
+    subject: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    }
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+contactSchema.plugin(softDeletePlugin);
+
+export const Contact = model<TContact, TContactModel>("Contact", contactSchema);
