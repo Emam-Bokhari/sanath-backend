f0d53c8f47c9b0d24f6084ad0b1ec71ae43d44ab
Moshfiqur Rahman <studentemam@gmail.com>
2026-06-09 10:02:45 +0600

    feat: add contact interface

---
+++ src/app/modules/contact/contact.interface.ts
@@ -0,0 +1,12 @@
+import { Types } from "mongoose";
+import { ISoftDeleteModel } from "../../../types/softDelete";
+
+export type TContact = {
+  userId: Types.ObjectId;
+  name: string;
+  email?: string;
+  subject: string;
+  message: string;
+};
+
+export type TContactModel = ISoftDeleteModel<TContact>;
